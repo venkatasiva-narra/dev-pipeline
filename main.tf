1ada22291e46db9990c1ae04626f6e24cc9bea32
venkatasiva-narra <75665980+venkatasiva-narra@users.noreply.github.com>
2021-06-04 10:51:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,15 @@
-provider "aws" {
-    profile = "default"
-    region = "us-west-2"
-    
-}
- 
-resource "aws_instance" "git-terra-jen" {
-    ami     = "ami-0cf6f5c8a62fa5da6"
-    tenancy = "default"
-    instance_type = "t2.micro"
+  
+terraform {
+  backend "azurerm" {
+  }
 }
 
+provider "azurerm" {
+  version = ">=2.0"
+  features {}
+}
 
-resource "aws_vpc" "venkat_vpc"{
-    cidr_block = "190.160.0.0/16"
-    instance_tenancy = "default"
-
-    tags = {
-        Name = "venkat_vpc"
-        
-
-    }
+resource "azurerm_resource_group" "rg" {
+  name     = "resourcegroup-test-tbd-3"
+  location = "westeurope"
 }
